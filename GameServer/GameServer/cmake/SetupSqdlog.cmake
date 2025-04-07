bf520058693377eded07ca04861cae953e344bd9
RubyDuDo <candyreddh@gmail.com>
2025-04-07 12:34:04 -0700

    update comments

--- GameServer/GameServer/cmake/SetupSqdlog.cmake
+++ GameServer/GameServer/cmake/SetupSqdlog.cmake
@@ -9,15 +9,12 @@ FetchContent_Declare(
 # FetchContent_MakeAvailable is a macro that adds the target spdlog to the current CMake project.
 FetchContent_MakeAvailable(spdlog)
 
-# 获取 protobuf 的源路径
+# add the include path
 FetchContent_GetProperties(spdlog)
-
-# 添加 protobuf 的 src 目录到你的项目 include 路径，解决 port_def.inc 找不到的问题
 target_include_directories(GameServer PRIVATE
   ${spdlog_SOURCE_DIR}/../include
 )
-
-
+# add the library
 target_link_libraries(GameServer PRIVATE spdlog::spdlog)
 
 
